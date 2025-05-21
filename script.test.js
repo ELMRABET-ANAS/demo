ff38cfdc9fd5220ece483567e729cd901b663c4b
ELMRABET <anas.elmrabet@ilemgroup.com>
2025-05-21 10:42:57 +0100

    test changed

--- script.test.js
+++ script.test.js
@@ -1,7 +1,7 @@
 const { add, subtract } = require('./script');
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(add(1, 2)).toBe(3);
+  expect(add(1, 2)).toBe(5);
 });
 
 test('subtracts 5 - 3 to equal 2', () => {
